6788575311d2d3a7bdb8de1c22cf8e36fc3687e4
Hashmi <digiacomo93@gmail.com>
2024-09-10 05:45:10 +0200

    turn create player function into an iife

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const Gameboard = (function () {
-    const createGameboard = () => {
+    createGameboard = () => {
         const rows = 3;
         const columns = 3;
         const matrix = Array(rows)
@@ -10,33 +10,36 @@ const Gameboard = (function () {
     return { createGameboard }
 })();
 
-function createPlayer(name, marker) {
-    let score = 0;
-    const makeMove = () => {
-        let validMove = false;
-        while (!validMove) {
-            let move = prompt(`${name}, make your move (row and column)!`);
-            let fixMove = move.split('').map(Number);
-            if (fixMove.length === 2 &&
-                fixMove[0] >= 0 && fixMove[0] <= 2 &&
-                fixMove[1] >= 0 && fixMove[1] <= 2 &&
-                gameboard[fixMove[0]][fixMove[1]] === 0) {
+const Player = (function () {
+    createPlayer = (name, marker) => {
+        let score = 0;
+        makeMove = () => {
+            let validMove = false;
+            while (!validMove) {
+                let move = prompt(`${name}, make your move (row and column)!`);
+                let fixMove = move.split('').map(Number);
+                if (fixMove.length === 2 &&
+                    fixMove[0] >= 0 && fixMove[0] <= 2 &&
+                    fixMove[1] >= 0 && fixMove[1] <= 2 &&
+                    gameboard[fixMove[0]][fixMove[1]] === 0) {
 
-                gameboard[fixMove[0]][fixMove[1]] = marker;
-                validMove = true;
+                    gameboard[fixMove[0]][fixMove[1]] = marker;
+                    validMove = true;
 
-            }
-            else {
-                alert('Warning, move not allowed. Try again.');
-            }
+                }
+                else {
+                    alert('Warning, move not allowed. Try again.');
+                }
 
-        };
+            };
+        }
+        return { score, makeMove, name, marker }
     }
-    return { name, marker, score, makeMove };
-}
+    return { createPlayer };
+})();
 
-const player1 = createPlayer('Hashmi', 'x');
-const player2 = createPlayer('Antonio', 'o');
+const player1 = Player.createPlayer('Hashmi', 'x');
+const player2 = Player.createPlayer('Antonio', 'o');
 const gameboard = Gameboard.createGameboard();
 
 const functionGame = (function (player1, player2, gameboard) {
