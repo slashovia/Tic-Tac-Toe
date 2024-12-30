32fc633307094e0511fdc43eb50b434915a191f0
Hashmi <digiacomo93@gmail.com>
2024-12-30 18:04:20 +0100

    Put functions before return

--- src/js/gameBoardManager.js
+++ src/js/gameBoardManager.js
@@ -10,52 +10,57 @@ export const gameBoardManager = function () {
             mouse.out(event.target)
         }
     }
+    function cellEvents(event) {
+        if (cellEvents[event.type]) {
+            cellEvents[event.type](event)
+        }
+    }
+    function createBoard() {
+        const gameBoard = document.querySelector('.gameboard')
+        for (let i = 0; i < 9; i++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            gameBoard.appendChild(cell);
+            const input = document.createElement('input');
+            input.type = 'button';
+            cell.appendChild(input);
+        }
+        cells = gameBoard.querySelectorAll('.cell')
+    }
 
+    function handlerCellEvents(event) {
+        if (cellEvents[event.type]) {
+            cellEvents[event.type](event)
+        }
+    }
+    function winnerCells([a, b, c]) {
+        [a, b, c].forEach(i => {
+            cells[i].style.transform = '';
+            cells[i].style.backgroundColor = '#F5CB58';
+        })
+    }
+    function resetCells() {
+        cells.forEach(c => {
+            c.textContent = '';
+            c.style.backgroundColor = '';
+            c.style.border = '';
+            c.style.color = '';
+        })
+    }
+    function removeHoverEvents() {
+        cells.forEach(c => {
+            c.removeEventListener('mouseover', handlerCellEvents);
+            c.removeEventListener('mouseout', handlerCellEvents)
+        }
+        )
+    }
     return {
-        createBoard: function () {
-            const gameBoard = document.querySelector('.gameboard')
-            for (let i = 0; i < 9; i++) {
-                const cell = document.createElement('div');
-                cell.classList.add('cell');
-                gameBoard.appendChild(cell);
-                const input = document.createElement('input');
-                input.type = 'button';
-                cell.appendChild(input);
-            }
-            cells = gameBoard.querySelectorAll('.cell')
-        },
-        handlerCellEvents: function (event) {
-            if (cellEvents[event.type]) {
-                cellEvents[event.type](event)
-            }
-        },
-        winnerCells: function ([a, b, c]) {
-            [a, b, c].forEach(i => {
-                cells[i].style.transform = '';
-                cells[i].style.backgroundColor = '#F5CB58';
-            })
-        },
-        resetCells: function () {
-            cells.forEach(c => {
-                c.textContent = '';
-                c.style.backgroundColor = '';
-                c.style.border = '';
-                c.style.color = '';
-            })
-        },
-        cellEvents: function (event) {
-            if (cellEvents[event.type]) {
-                cellEvents[event.type](event)
-            }
-        },
-
-        removeHoverEvents: function () {
-            cells.forEach(c => {
-                c.removeEventListener('mouseover', handlerCellEvents);
-                c.removeEventListener('mouseout', handlerCellEvents)
-            }
-            )
-        },
+        createBoard,
+        handlerCellEvents,
+        winnerCells,
+        resetCells,
+        cellEvents,
+        removeHoverEvents
     }
 
 }
